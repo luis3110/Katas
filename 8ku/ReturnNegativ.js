f88468d74772170f76ad2f34ef7755fab69a0301
luisschmitz <luis.schmitz@web.de>
2020-08-03 17:35:44 +0200

    first commit

---
+++ 8ku/ReturnNegativ.js
@@ -0,0 +1,20 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Example:
+
+// makeNegative(1); // return -1
+// makeNegative(-5); // return -5
+// makeNegative(0); // return 0
+// makeNegative(0.12); // return -0.12
+
+function makeNegative(num) {
+    // Code?
+    if(num > 0){
+      return -(num);
+    } else return num
+  }
+  
+  makeNegative(-42) 
+  // -42);
+  
+  // return number with - in front
